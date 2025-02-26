d237099d6bf377154cc52b70ced945a1fc21374e
B.Karthikeya <148749011+BendiKarthikeya@users.noreply.github.com>
2025-02-26 11:47:30 +0530

    Time: 11 ms (65.6%), Space: 25.2 MB (33.06%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int n = arr.size();
+        int sum = 0;
+        for (int num : arr) sum += num;
+
+        if (target > sum || target < -sum) return 0;
+
+        vector<vector<int>> dp(n, vector<int>(2*sum + 1, -1));
+        return ans(n - 1, 0,target, arr, dp,sum);
+    }
+    int ans(int i, int j,int target ,vector<int>& arr, vector<vector<int>>& dp,int sum) {
+        if (i < 0) return j==target?1:0;
+        
+        if (dp[i][j+sum] == -1){
+            int exclude = ans(i - 1, j + arr[i],target,arr, dp,sum); 
+            int include = ans(i - 1, j -arr[i],target,arr, dp,sum);             
+            dp[i][j+sum] = include + exclude; 
+        } 
+        return dp[i][j+sum]; 
+    }
+};
