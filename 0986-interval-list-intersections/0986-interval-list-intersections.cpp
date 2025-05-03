510f7f6c156d0c838296bc5a50a004e2b3d6af60
B.Karthikeya <148749011+BendiKarthikeya@users.noreply.github.com>
2025-05-03 22:24:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> ans;
+        for(int i=0;i<firstList.size());i++){
+            curri=firstList[i][0];
+            curri=scondList[i][0];
+            for(int j=0;j<secondList.size();j++){
+                if(max())
+            }
+        }
+    }
+};
