158303ff0fcda599ae661bb22b433233ce824cd9
B.Karthikeya <148749011+BendiKarthikeya@users.noreply.github.com>
2025-05-31 01:25:16 +0530

    Time: 16 ms (61.11%), Space: 47.3 MB (25.46%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> p2(n,n);
         stack<int> st;
         for(int i=0;i<n;i++){
-            while(st.size()>0 && arr[st.top()]>=arr[i]){
+            while(st.size()>0 && arr[st.top()]>arr[i]){
                 st.pop();
             }
             if(st.size()>0){
